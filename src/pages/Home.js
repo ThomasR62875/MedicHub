0f9cd73fa2ed1a09954955317066e3cff74c300f
jtechenski <jtechenski@itba.edu.ar>
2024-04-07 16:22:23 -0300

    setting up supabase and organizing folders

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import supabase from "../config/supabaseClient";
+
+
+const Home = () => {
+    console.log(supabase)
+
+    return (
+        <div className="page home">
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
