3e2294a51c6258069db327c8fad1a2ce01d448a7
jtechenski <jtechenski@itba.edu.ar>
2024-04-11 17:54:47 -0300

    working navigation to home screen

---
+++ Navigation.tsx
@@ -0,0 +1,21 @@
+// Navigation.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen'; // Aquí deberías importar tu pantalla de inicio de sesión
+
+const Stack = createStackNavigator();
+
+const Navigation: React.FC = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Auth from '../components/Auth'; // Importa tu componente Auth existente
+
+const LogInScreen: React.FC = () => {
+    return (
+        <Auth />
+    );
+};
+
+export default LogInScreen;
