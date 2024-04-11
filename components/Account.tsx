6ee01f450a57c982434e362ff0954955495a6cb5
jtechenski <jtechenski@itba.edu.ar>
2024-04-11 00:53:16 -0300

    adding files not added

---
+++ components/Account.tsx
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+import {supabase} from "../lib/supabase";
+import {Alert, StyleSheet, View} from "react-native";
+import {Button, Input} from "react-native-elements";
+
+export default function Account(){
+
+}
