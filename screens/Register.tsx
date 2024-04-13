d12d2887dbbf9d194368fc7355621164c00060c4
valentinamarti <vmartireta@itba.edu.ar>
2024-04-13 15:46:45 -0300

    Commit changes for Register.tsx

--- screens/Register.tsx
+++ screens/Register.tsx
@@ -7,6 +7,9 @@ const Register: React.FC = ({ navigation }: any) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [loading, setLoading] = useState(false)
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [dni, setDni] = useState('')
 
     async function signUpWithEmail() {
         setLoading(true)
@@ -16,6 +19,13 @@ const Register: React.FC = ({ navigation }: any) => {
         } = await supabase.auth.signUp({
             email: email,
             password: password,
+            options: {
+                data: {
+                    firs_name: firstName,
+                    last_name: lastName,
+                    dni: dni,
+                },
+            },
         })
 
         if (error) Alert.alert(error.message)
@@ -26,6 +36,38 @@ const Register: React.FC = ({ navigation }: any) => {
         <View style={styles.container}>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>Register Screen</Text>
+            </View>
+            <View style={styles.horizontallySpaced}>
+                <View style={styles.nameContainer}>
+                    <Input
+                        label="First Name"
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                        onChangeText={(text) => setFirstName(text)}
+                        value={firstName}
+                        placeholder="First Name"
+                        autoCapitalize={'none'}
+                        containerStyle={styles.inputContainer} // Add inputContainer style
+                    />
+                    <Input
+                        label="Last Name"
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                        onChangeText={(text) => setLastName(text)}
+                        value={lastName}
+                        placeholder="Last Name"
+                        autoCapitalize={'none'}
+                        containerStyle={styles.inputContainer} // Add inputContainer style
+                    />
+                </View>
+            </View>
+            <View style={styles.verticallySpaced}>
+                <Input
+                    label="DNI"
+                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                    onChangeText={(text) => setDni(text)}
+                    value={dni}
+                    placeholder="DNI"
+                    autoCapitalize={'none'}
+                />
             </View>
             <View style={[styles.verticallySpaced, styles.mt20]}>
                 <Input
@@ -68,19 +110,28 @@ const Register: React.FC = ({ navigation }: any) => {
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 200,
+        marginTop: 30,
         padding: 12,
     },
     verticallySpaced: {
-        paddingTop: 4,
-        paddingBottom: 4,
+        paddingTop: 2,
+        paddingBottom: 2,
         alignSelf: 'stretch',
     },
     mt20: {
-        marginTop: 20,
+        marginTop: 5,
     },
-
-})
+    horizontallySpaced: {
+        paddingTop: 2,
+        paddingBottom: 2,
+    },
+    nameContainer: {
+        flexDirection: 'row', // Display first name and last name in a row
+    },
+    inputContainer: {
+        width: '50%', // Make each input field take up 50% of the container width
+    },
+});
 
 export default Register;
 
