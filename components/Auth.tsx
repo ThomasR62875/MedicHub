86350909ba9c471b6410eea5c7b1c2a048142c7e
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-15 11:17:01 -0300

    added remove keyboard option to Auth.tsx

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import {Alert, StyleSheet, View, Dimensions, AppState, Text} from 'react-native'
+import {Alert, StyleSheet, View, Dimensions, AppState, Text, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import { supabase } from '../lib/supabase'
 import { Button, Input } from 'react-native-elements'
 import { useNavigation, ParamListBase } from '@react-navigation/native'; // Importa useNavigation desde @react-navigation/native
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {BrowserRouter} from "react-router-dom";
+//import {BrowserRouter} from "react-router-dom";
 
 const windowHeight = Dimensions.get('window').height;
 
@@ -35,44 +35,46 @@ export default function Auth() {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={[styles.inputContainer, { height: windowHeight * 0.08 }]}>
-                <Input
-                    label="Mail"
-                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    placeholder=" email@address.com"
-                    autoCapitalize={'none'}
-                    inputContainerStyle={[{paddingLeft: 10}, styles.input]}
-                />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style={styles.container}>
+                <View style={[styles.inputContainer, { height: windowHeight * 0.08 }]}>
+                    <Input
+                        label="Mail"
+                        leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                        onChangeText={(text) => setEmail(text)}
+                        value={email}
+                        placeholder=" email@address.com"
+                        autoCapitalize={'none'}
+                        inputContainerStyle={[{paddingLeft: 10}, styles.input]}
+                    />
+                </View>
+                <View style={[styles.inputContainer, {height: windowHeight * 0.08 }]}>
+                    <Input
+                        label="Contraseña"
+                        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                        onChangeText={(text) => setPassword(text)}
+                        value={password}
+                        secureTextEntry={true}
+                        placeholder=" Contraseña"
+                        autoCapitalize={'none'}
+                        inputContainerStyle={[{paddingLeft: 10}, styles.input]}
+                    />
+                </View>
+                <View style={[styles.buttonSignInContainer, { height: windowHeight * 0.08 }]}>
+                    <Button title="Ingresar" disabled={loading} onPress={() => signInWithEmail()} buttonStyle={[styles.buttonSignIn, { backgroundColor: '#3EB77F' }]} />
+                </View>
+                <View style={[styles.buttonRegisterContainer, { marginTop: 80, height: windowHeight * 0.08 }]}>
+                    <Text style={{color: '#000000', textAlign: 'center', fontSize: 18}}> ¿No tenes una cuenta?
+                    </Text>
+                    <Button title="Registrate"
+                            disabled={loading}
+                            onPress={() => navigation.navigate('Register')}
+                            buttonStyle={[styles.buttonRegister]}
+                            titleStyle={{color: '#000000', textDecorationLine: 'underline'}}
+                    />
+                </View>
             </View>
-            <View style={[styles.inputContainer, {height: windowHeight * 0.08 }]}>
-                <Input
-                    label="Contraseña"
-                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                    onChangeText={(text) => setPassword(text)}
-                    value={password}
-                    secureTextEntry={true}
-                    placeholder=" Contraseña"
-                    autoCapitalize={'none'}
-                    inputContainerStyle={[{paddingLeft: 10}, styles.input]}
-                />
-            </View>
-            <View style={[styles.buttonSignInContainer, { height: windowHeight * 0.08 }]}>
-                <Button title="Ingresar" disabled={loading} onPress={() => signInWithEmail()} buttonStyle={[styles.buttonSignIn, { backgroundColor: '#3EB77F' }]} />
-            </View>
-            <View style={[styles.buttonRegisterContainer, { marginTop: 80, height: windowHeight * 0.08 }]}>
-                <Text style={{color: '#000000', textAlign: 'center', fontSize: 18}}> ¿No tenes una cuenta?
-                </Text>
-                <Button title="Registrate"
-                        disabled={loading}
-                        onPress={() => navigation.navigate('Register')}
-                        buttonStyle={[styles.buttonRegister]}
-                        titleStyle={{color: '#000000', textDecorationLine: 'underline'}}
-                />
-            </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
 
