fa0641e15ce64d893946b6c8442d6959c6d0d8b7
ruijg <truijgt@itba.edu.ar>
2024-04-24 10:26:30 -0300

    detalles

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -7,24 +7,25 @@ import {Card} from '../components/Card';
 const Home: React.FC = ({navigation}: any) => {
 
     const [first_name, setFirstName] = useState('')
-
+    const imgDoc = '../assets/doc.png';
     return (
         <View>
             <View style={styles.profileIconContainer} >
                 <Icon name='person-circle-outline' type='ionicon' onPress={() => navigation.navigate('Account')} size={35} />
             </View>
             <View style={styles.container}>
+                {/* NOSE DONDE MIERDA MÁS PONERLE CENTER PARA Q LAS CARDS APAREZCAN BIEN miren los styles van a entender todo*/}
                 <Text style={styles.text}>Bienvenido {first_name} test!!</Text>
                 <View style={styles.grid}>
                     <View style={styles.col}>
-                        <Card title="Agregar turno" img="../assets/splash.png"  onPress={() => navigation.navigate('AddAppointment')} />
+                        <Card title="Agregar turno" img={imgDoc}  onPress={() => navigation.navigate('AddAppointment')} />
                         <View style={{ marginBottom: 10 }} />
-                        <Card title="Turnos" img="../assets/splash.png" onPress={() => navigation.navigate('Appointments')}/>
+                        <Card title="Turnos" img={imgDoc} onPress={() => navigation.navigate('Appointments')}/>
                     </View>
                     <View style={styles.col}>
-                        <Card title="Agregar doctor" img="../assets/doc.png"  onPress={() => navigation.navigate('AddDoctor')}/>
+                        <Card title="Agregar doctor" img={imgDoc} onPress={() => navigation.navigate('AddDoctor')}/>
                         <View style={{ marginBottom: 10 }} />
-                        <Card title="Doctors" img="../assets/doc.png"  onPress={() => navigation.navigate('Doctors')}/>
+                        <Card title="Doctors" img={imgDoc}  onPress={() => navigation.navigate('Doctors')}/>
                     </View>
                 </View>
             </View>
@@ -34,8 +35,8 @@ const Home: React.FC = ({navigation}: any) => {
 
 const styles = StyleSheet.create({
     profileIconContainer: {
-        top: 5,
-        left: 310,
+        top: 10,
+        left: 315,
         width: 50,
         height: 50,
         borderRadius: 25,
@@ -44,23 +45,24 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     container: {
-        padding: 10,
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: 'center',
     },
     grid: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 10,
     },
     col: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         marginRight: 10,
     },
     text: {
         fontSize: 25,
+        marginBottom: 5,
     }
 });
 
