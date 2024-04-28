07e8075bae207dc031fac0c17a597aced4b91d33
valentinamartireta <vmartireta@itba.edu.ar>
2024-04-28 16:52:58 -0300

    commit sql functions

--- screens/Appointments.tsx
+++ screens/Appointments.tsx
@@ -14,6 +14,7 @@ type AppointmentsProps = NativeStackScreenProps<RootStackParamList, 'Appointment
 export type Appointment = {
     date: Date;
     description:string;
+    user_name:string;
 }
 
 const Appointments: React.FC<AppointmentsProps> = ({ navigation, route }) => {
@@ -31,23 +32,36 @@ const Appointments: React.FC<AppointmentsProps> = ({ navigation, route }) => {
             setLoading(true)
             if (!session?.user) throw new Error('No user on the session!')
 
-            const {data, error, status} = await supabase
-                .from('appointment')
-                .select('id, date,description,doctor,user')
-                .eq('user', session?.user.id)
+            const {data: user_id,error: user_data_error} = await supabase.rpc('get_independent_user_id')
+            if(user_data_error)
+                throw new Error(user_data_error.message);
+
+            const {data, error, status} = await supabase.rpc('get_appointments', {user_id: user_id})
             if (error && status !== 406) {
                 throw error
             }
 
             if (data) {
-                data.forEach(appoint => {
-                    to_return.push({
-                        description: appoint.description,
-                        date: appoint.date,
-                    })
-                });
+                for (const appoint of data) {
+                    try {
+                        const { data: user_data, error: user_error } = await supabase.rpc('get_user', {user_id: appoint.user})
+
+                        if (user_error) {
+                            throw user_error;
+                        }
+
+                        // Aquí puedes hacer lo que necesites con user_data
+                        // Por ejemplo, agregarlo a to_return
+                        to_return.push({
+                            description: appoint.description,
+                            date: appoint.date,
+                            user_name: user_data.first_name // Suponiendo que name es el campo que quieres agregar
+                        });
+                    } catch (error) {
+                        console.error('Error al obtener el usuario:', error);
+                    }
+                }
             }
-        
         } catch (error) {
             if (error instanceof Error) {
                 Alert.alert(error.message)
@@ -64,16 +78,17 @@ const Appointments: React.FC<AppointmentsProps> = ({ navigation, route }) => {
                     </View>
                     <View>
                         {
-                        appointments.map((appoint:Appointment,i)=> {
-                            return(
-                                <View key="{i}" style={styles.appointmentView}>
-                                    <Text key="{appoint.date}{i}" style={styles.appointmentViewText}>{appoint.date.getDate()}</Text>
-                                    <Text key="{appoint.description}{i}" style={styles.appointmentViewText}>{appoint.description}</Text>
-                                </View>
-                            )
-                        })
+                            appointments.map((appoint:Appointment,i)=> {
+                                return(
+                                    <View key={i} style={styles.appointmentView}>
+                                        <Text style={styles.appointmentViewText}>{appoint.description}</Text>
+                                        <Text style={styles.appointmentViewText}>{appoint.user_name}</Text>
+                                    </View>
+                                )
+                            })
                         }
                     </View>
+
                     <View>
                         <Button title="Add Appointment"
                                 onPress={() => navigation.navigate('AddAppointment', {session: session})}
