0d93e7ab6732a4d30ad667e6118c0b9dbe88e7ff
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-29 16:46:55 -0300

    places title in Medication.tsx

--- screens/Medication.tsx
+++ screens/Medication.tsx
@@ -1,7 +1,7 @@
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../App";
 import React from "react";
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 
 
 type MedicationProps = NativeStackScreenProps<RootStackParamList, 'Medication'>;
@@ -10,10 +10,55 @@ type MedicationProps = NativeStackScreenProps<RootStackParamList, 'Medication'>;
 const Medication: React.FC<MedicationProps> = ({ navigation, route }) =>{
     const {session} = route.params;
     return(
-        <View style={{marginTop: 50, alignItems: 'center'}}>
-            <Text>Para el sprint 3</Text>
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>Medicamentos</Text>
+            </View>
+            <View style={{marginTop: 50, alignItems: 'center'}}>
+                <Text>Proximamente en el sprint 3</Text>
+            </View>
         </View>
     )
 }
 
 export default Medication;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+    },
+    medContainer: {
+        marginTop: 10,
+        backgroundColor: '#C2E5D3',
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    infoRow: {
+        flexDirection: 'row',
+        marginBottom: 5,
+    },
+    label: {
+        fontWeight: 'bold',
+        marginRight: 5,
+    },
+    value: {
+        flex: 1,
+    },
+    titleContainer: {
+        alignSelf: 'center',
+        marginBottom: 20,
+    },
+    titleText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    addContainer: {
+        left: 290,
+        bottom: 60,
+        alignSelf: 'flex-start',
+    }
+
+});
