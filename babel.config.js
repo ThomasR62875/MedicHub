cf5a96c49ad6f4f51953490d35c74d48e3a3b6a7
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-05-11 16:19:29 -0300

    addded tests directory and Login test file

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}], // Configuración para entorno de Node.js
+      '@babel/preset-expo', // Preset de Expo para React Native
+      '@babel/preset-typescript', // Preset de TypeScript
+    ],
+    plugins: [
+      ['module:react-native-dotenv'], // Si estás usando variables de entorno
+    ],
   };
 };
