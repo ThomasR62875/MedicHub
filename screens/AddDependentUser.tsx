b9db3b4fcbe6c721e84fd51dc383bdfa202a62ff
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-05-13 20:40:17 -0300

    design AddDependentUser.tsx

--- screens/AddDependentUser.tsx
+++ screens/AddDependentUser.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {SafeAreaView, Alert, StyleSheet, View,} from 'react-native';
 import {supabase} from "../lib/supabase";
-import {Input} from "react-native-elements";
+import {Button, Icon, Input, Text} from "react-native-elements";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../App";
 import { Image } from 'react-native';
@@ -37,31 +37,64 @@ const AddDependentUser: React.FC<AddDependentUser> = ({ navigation, route }) => 
 
     return (
         <View style={styles.container}>
-          <Input
-            leftIcon={{ type: 'font-awesome', name: 'user' }}
-            style={styles.verticallySpaced}
-            placeholder="Nombre"
-            value={firstName}
-            onChangeText={(text) => setFirstName(text)}
-          />
-        <Input
-            leftIcon={{ type: 'font-awesome', name: 'user' }}
-            style={styles.verticallySpaced}
-            placeholder="Apellido"
-            value={lastName}
-            onChangeText={(text) => setLastName(text)}
-          />
-            <Input
-            leftIcon={<Image source={require('../assets/fingerprint.png')} style={styles.icon} />}
-            style={styles.verticallySpaced}
-            placeholder="DNI"
-            value={dni}
-            onChangeText={(text) => setDni(text)}
-            />
-            <StandardGreenButton title="Confirmar"
-                                 disabled={loading}
-                                 onPress={() => addUser()}
-            />
+            <View style={styles.window}>
+            <Text style={styles.screenTitle}>Nuevo Usuario</Text>
+                <Input
+                    label="Nombre"
+                    labelStyle={styles.colorLable}
+                    leftIcon={<Icon type="material-icons" name="person" color={styles.colorLable.color}/>}
+                    onChangeText={(text) => setFirstName(text)}
+                    value={firstName}
+                    secureTextEntry={true}
+                    placeholder="Nombre"
+                    autoCapitalize={'none'}
+                    inputStyle={{color: '#407738', marginLeft: 10}}
+                    placeholderTextColor={"#407738"}
+                />
+                <Input
+                    label="Apellido"
+                    labelStyle={styles.colorLable}
+                    leftIcon={<Icon type="material-icons" name="person" color={styles.colorLable.color}/>}
+                    onChangeText={(text) => setLastName(text)}
+                    value={lastName}
+                    secureTextEntry={true}
+                    placeholder="Apellido"
+                    autoCapitalize={'none'}
+                    inputStyle={{color: '#407738', marginLeft: 10}}
+                    placeholderTextColor={"#407738"}
+                />
+                <Input
+                    label="DNI"
+                    labelStyle={styles.colorLable}
+                    leftIcon={<Icon type="material-icons" name="fingerprint" color={styles.colorLable.color}/>}
+                    onChangeText={(text) => setDni(text)}
+                    value={dni}
+                    secureTextEntry={true}
+                    placeholder="DNI"
+                    autoCapitalize={'none'}
+                    inputStyle={{color: '#407738', marginLeft: 10}}
+                    placeholderTextColor={"#407738"}
+                />
+                <Button
+                    title="Agregar"
+                    loading={loading}
+                    buttonStyle={{
+                        backgroundColor: '#2E5829',
+                        borderWidth: 2,
+                        borderColor: 'white',
+                        borderRadius: 30,
+                        minHeight: 50
+                    }}
+                    containerStyle={{
+                        width: 150,
+                        marginHorizontal: 50,
+                        marginVertical: 10,
+                        marginTop: 40,
+                    }}
+                    titleStyle={{ color: '#eef9ed' }}
+                    onPress={() => addUser()}
+                />
+            </View>
         </View>
       );
 }
@@ -70,11 +103,8 @@ export default AddDependentUser;
 
 const styles = StyleSheet.create({
     container: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 30,
-      padding: 12,
-      backgroundColor: '#e9f4e9',
+        alignItems: 'center',
+        backgroundColor: '#e9f4e9', height: "100%"
     },
     containerTotal:{
         backgroundColor: '#e9f4e9',
@@ -102,5 +132,21 @@ const styles = StyleSheet.create({
     icon: {
         width: 24,
         height: 24,
+    }, screenTitle: {
+        fontFamily: 'Roboto-Thin',
+        fontSize: 25,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginTop: "1%",
+        marginBottom: "15%",
+        color: "#2E5829FF",
     },
-  });
+    window: {
+        alignItems: 'center',
+        marginTop: "20%",
+        width: "90%",
+    },
+    colorLable: {
+        color: '#2E5829FF',
+    },
+});
