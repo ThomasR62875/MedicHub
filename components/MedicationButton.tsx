ede0882b9048d7f46bf2e13c7f4f4486de6bd8f7
Maggie Taurian <mtaurian@itba.edu.ar>
2024-05-14 08:36:11 -0300

    created MedicationButton.tsx

---
+++ components/MedicationButton.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Icon} from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import {Medication} from "../screens/Medication";
+
+interface MedicationButtonProps {
+    //onPress: any;
+    styleExterior: any;
+    meds: Medication;
+}
+
+const MedicationButton: React.FC<MedicationButtonProps> = ( { styleExterior, meds }) => {
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity style={styles.container}>
+            <View style={styleExterior}>
+                <View style={styles.infoRow}>
+                    <Text style={styles.label}>Nombre:</Text>
+                    <Text>{meds.name}</Text>
+                </View>
+                <View style={styles.infoRow}>
+                    <Text style={styles.label}>Prescripción:</Text>
+                    <Text>{meds.prescription}</Text>
+                    <View style={{ width: 30 }} />
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        width: '90%',
+        height: 100,
+        borderRadius: 28,
+        backgroundColor: '#C2E5D3',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    infoRow: {
+        flexDirection: 'row',
+        marginBottom: 5,
+        padding: 5,
+    },
+    label: {
+        fontWeight: 'bold',
+        marginRight: 5,
+    },
+});
+
+export default MedicationButton;
