4dc401fede9514a967c4b7626e384535ecb126a6
valentinamartireta <vmartireta@itba.edu.ar>
2024-05-13 22:58:32 -0300

    commit implementation of actualization of data in Appointments.tsx

--- screens/Appointments.tsx
+++ screens/Appointments.tsx
@@ -17,10 +17,19 @@ const Appointments: React.FC =  ({navigation, route}: any) =>{
     const session = route.params.session;
     const [loading, setLoading] = useState(true)
     const [appointments,setAppointments]= useState<Appointment[] | undefined>(undefined)
+    const [sessionId, setSessionId] = useState('')
+
+
 
     useEffect(() => {
-        if (session) getAppointments()
-    }, [session])
+        if (session)
+            setSessionId(session);
+    }, [sessionId])
+
+
+    useEffect(() => {
+        if (sessionId) getAppointments()
+    }, [sessionId])
 
     async function getAppointments() {
         const to_return: Appointment[] = [];
@@ -64,6 +73,7 @@ const Appointments: React.FC =  ({navigation, route}: any) =>{
             }
         }
         setLoading(false)
+        setSessionId('')
         setAppointments(to_return)
     }
     return(
