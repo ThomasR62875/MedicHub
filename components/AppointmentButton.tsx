e355ba783ca7458d3d3df171ee0306240f96ff65
Maggie Taurian <mtaurian@itba.edu.ar>
2024-05-13 22:18:08 -0300

    created AppointmentButton.tsx component

---
+++ components/AppointmentButton.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Icon} from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import {Appointment} from "../screens/Appointments";
+
+interface AppointmentButtonProps {
+    onPress: any;
+    styleExterior: any;
+    date : Date;
+    turno: Appointment;
+}
+
+const AppointmentButton: React.FC<AppointmentButtonProps> = ( { styleExterior, turno, date, onPress }) => {
+    const navigation = useNavigation();
+    const otraDate = new Date(date);
+    return (
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+            <View style={styleExterior}>
+                <View style={styles.infoRow}>
+                    <Text style={{textAlign: 'center'}}>{turno.description}</Text>
+                </View>
+                <View style={styles.infoRow}>
+                    <Text style={styles.label}>Usuario:</Text>
+                    <Text>{turno.user_name}</Text>
+                    <View style={{ width: 30 }} />
+                    <Text style={styles.label}>Fecha:</Text>
+                    <Text>{otraDate.getDate()}/{otraDate.getMonth()+1}/{otraDate.getFullYear()}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        width: '90%',
+        height: 100,
+        borderRadius: 28,
+        backgroundColor: '#C2E5D3',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    infoRow: {
+        flexDirection: 'row',
+        marginBottom: 5,
+        padding: 5,
+    },
+    label: {
+        fontWeight: 'bold',
+        marginRight: 5,
+    },
+});
+
+export default AppointmentButton;
