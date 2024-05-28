7014065590e0d51209147f8ca1a25adfcfc86d3f
Maggie Taurian <mtaurian@itba.edu.ar>
2024-05-28 08:34:45 -0300

    deleted picker style

--- screens/EditDoctor.tsx
+++ screens/EditDoctor.tsx
@@ -299,26 +299,3 @@ const styles = StyleSheet.create({
         width: "70%"
     }
 })
-
-const pickerSelectStyles = StyleSheet.create({
-    inputIOS: {
-        fontSize: 16,
-        paddingVertical: 12,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 4,
-        color: 'black',
-        paddingRight: 30,
-    },
-    inputAndroid: {
-        fontSize: 16,
-        paddingHorizontal: 10,
-        paddingVertical: 8,
-        borderWidth: 0.5,
-        borderColor: 'purple',
-        borderRadius: 8,
-        color: 'black',
-        paddingRight: 30,
-    },
-});
