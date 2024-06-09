9bb41baad5bf3aa5d00b4587fbdc40cf70828039
MatiasColeur <120046120+MatiasColeur@users.noreply.github.com>
2024-06-09 11:50:05 -0300

    add types file and all the types used in the proyect

---
+++ lib/types.ts
@@ -0,0 +1,49 @@
+
+
+export type DependentUser = {
+    first_name: string;
+    last_name: string;
+    dni: string;
+    id: string;
+}
+
+export type User = {
+    id: string;
+    first_name: string;
+    last_name: string;
+    dni: string;
+    email: string;
+}
+
+export type Specialty = {
+    name: string;
+};
+
+export type Medication = {
+    id: string;
+    name: string;
+    prescription: string;
+    sinceWhen : Date;
+    untilWhen : Date | null;
+    howOften : Date | null;
+    isForever : boolean;
+}
+
+export type Doctor = {
+    id:string;
+    name: string;
+    specialty: string;
+    phone: string;
+    email: string;
+    addresses: string[];
+    user_id:string;
+}
+
+export type Appointment = {
+    id: string;
+    date: Date;
+    description: string;
+    user_name: string;
+    doctor: string;
+    user_id: string;
+}
