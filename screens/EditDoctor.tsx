b38890968a4999d67073d4e90dde3b5c8e40ede6
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-07-13 20:52:36 -0300

    edit doctros header update

--- screens/EditDoctor.tsx
+++ screens/EditDoctor.tsx
@@ -121,32 +121,33 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
     };
     return (
         <View style={styles.tab}>
-            <Image source={Header} style={styles.header}/>
-
-            <Icon iconStyle={{color: 'white', paddingVertical: 20}} name={'arrow-left'} type={'material-community'}
-                  style={styles.back_arrow}
-                  onPress={() => navigation.navigate('SingleDoctor', {doc: doc})}></Icon>
-            <View style={{
-                flexDirection: 'row',
-                paddingTop: '5%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginRight: 2
-            }}>
-                <Icon iconStyle={{color: 'white', fontSize: 24}} containerStyle={[styles.circleHeader, {
-                    backgroundColor: 'rgba(134,171,186,0.6)',
-                    alignSelf: 'center',
-                    marginHorizontal: "10%"
-                }]} name={'stethoscope'} type={'material-community'}/>
+            <View style={[styles.header, {backgroundColor: 'rgba(134,171,186,0.6)'}]}>
+                <View style={{
+                    flexDirection: 'row',
+                    marginHorizontal: '10%',
+                    marginVertical: '20%',
+                    alignItems: 'flex-start',
+                }}>
+                    <Icon iconStyle={{color: 'white'}} name={'arrow-left'} type={'material-community'}
+                          style={styles.back_arrow}
+                          onPress={() => navigation.navigate('SingleDoctor', {doc: doc})}></Icon>
+                    <Icon iconStyle={{color: 'white', fontSize: 20}} containerStyle={[styles.circleHeader, {
+                        backgroundColor: 'rgba(134,171,186,0.6)',
+                        alignSelf: 'center',
+                        marginHorizontal: '35%'
+                    }]} name={'stethoscope'} type={'material-community'}/>
+                </View>
             </View>
+
             <ScrollableBg style={{padding: '10%'}}>
                 <Input
                     label={t('name')}
-                    leftIcon={<Icon type="font-awesome" name="user" color={styles.colorIcon.color}  iconStyle={{fontSize: 20, paddingLeft: 10}} />}
+                    leftIcon={<Icon type="font-awesome" name="user" color={styles.colorIcon.color}
+                                    iconStyle={{fontSize: 20, paddingLeft: 10}}/>}
                     labelStyle={styles.label2}
                     placeholderTextColor={"#807d7d"}
                     inputContainerStyle={[{paddingLeft: 10}, styles.input]}
-                    inputStyle={{color: '#000', fontSize:14, marginLeft: 10}}
+                    inputStyle={{color: '#000', fontSize: 14, marginLeft: 10}}
                     onChangeText={(text) => {
                         setName(text);
                         validateName(text);
@@ -158,8 +159,11 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
                     errorMessage={nameErrorMessage}
                 />
 
-                <PaperText style={[styles.label2,{paddingLeft: 14}]}>{t('specialty')}</PaperText>
-                <PaperButton mode="outlined" style={[styles.input, {padding: 5, marginHorizontal: '3.5%', marginBottom:'5%'}]} textColor='#000' labelStyle={{textAlign: 'left', display:'flex'}} contentStyle={{justifyContent: 'flex-start'}} onPress={() => setSpecialtyDialog(true)}>
+                <PaperText style={[styles.label2, {paddingLeft: 14}]}>{t('specialty')}</PaperText>
+                <PaperButton mode="outlined"
+                             style={[styles.input, {padding: 5, marginHorizontal: '3.5%', marginBottom: '5%'}]}
+                             textColor='#000' labelStyle={{textAlign: 'left', display: 'flex'}}
+                             contentStyle={{justifyContent: 'flex-start'}} onPress={() => setSpecialtyDialog(true)}>
                     {t(specialty)}
                 </PaperButton>
 
@@ -173,7 +177,7 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
                     labelStyle={styles.label2}
                     placeholderTextColor={"#807d7d"}
                     inputContainerStyle={[{paddingLeft: 10}, styles.input]}
-                    inputStyle={{color: '#000', fontSize:14, marginLeft: 10}}
+                    inputStyle={{color: '#000', fontSize: 14, marginLeft: 10}}
                 />
 
                 <Input
@@ -186,7 +190,7 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
                     labelStyle={styles.label2}
                     placeholderTextColor={"#807d7d"}
                     inputContainerStyle={[{paddingLeft: 10}, styles.input]}
-                    inputStyle={{color: '#000', fontSize:14, marginLeft: 10}}
+                    inputStyle={{color: '#000', fontSize: 14, marginLeft: 10}}
                 />
 
                 <Input
@@ -199,11 +203,14 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
                     labelStyle={styles.label2}
                     placeholderTextColor={"#807d7d"}
                     inputContainerStyle={[{paddingLeft: 10}, styles.input]}
-                    inputStyle={{color: '#000', fontSize:14, marginLeft: 10}}
+                    inputStyle={{color: '#000', fontSize: 14, marginLeft: 10}}
                 />
 
-                <PaperText style={[styles.label2,{paddingLeft: 14}]}>{t("user")}</PaperText>
-                <PaperButton mode="outlined" style={[styles.input, {padding: 5, marginHorizontal: '3.5%', marginBottom:'5%'}]} textColor='#000' labelStyle={{textAlign: 'left', display:'flex'}} contentStyle={{justifyContent: 'flex-start'}} onPress={() => setUserDialog(true)}>
+                <PaperText style={[styles.label2, {paddingLeft: 14}]}>{t("user")}</PaperText>
+                <PaperButton mode="outlined"
+                             style={[styles.input, {padding: 5, marginHorizontal: '3.5%', marginBottom: '5%'}]}
+                             textColor='#000' labelStyle={{textAlign: 'left', display: 'flex'}}
+                             contentStyle={{justifyContent: 'flex-start'}} onPress={() => setUserDialog(true)}>
                     {getUserName(user_id)}
                 </PaperButton>
                 <Button
@@ -238,7 +245,7 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
                         itemStyle={styles.pickerStyle}
                     >
                         {all_users?.map((item) => (
-                            <Picker.Item key={item.id} label={item.first_name} value={item.id} />
+                            <Picker.Item key={item.id} label={item.first_name} value={item.id}/>
                         ))}
                     </Picker>
                 </Dialog>
@@ -253,7 +260,7 @@ const EditDoctor: React.FC<EditDoctorProps> = ({navigation, route}: any) => {
                         itemStyle={styles.pickerStyle}
                     >
                         {specialties?.map((item) => (
-                            <Picker.Item key={item.name} label={t(item.name)} value={item.name} />
+                            <Picker.Item key={item.name} label={t(item.name)} value={item.name}/>
                         ))}
                     </Picker>
                 </Dialog>
